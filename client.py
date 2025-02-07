36cfec64026d44e62403fcbbcd8adce8257edadb
searchcenterbot <searchcenterbot@gmail.com>
2025-02-07 23:59:36 +0600

    Update client.py

--- client.py
+++ client.py
@@ -15,6 +15,6 @@ class Bot(Client):
             plugins={"root": "plugins"})
     async def start(self):                        
         await super().start()  
-        print("Bot Started 🔧 Powered By @VJ_Botz")   
+        print("Bot Started 🔧 Powered By @mr_terabox")   
     async def stop(self, *args):
         await super().stop()
