57a675bab5098029a9272c1c1d17594b0e9d0df9
searchcenterbot <searchcenterbot@gmail.com>
2025-02-07 23:54:13 +0600

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -18,8 +18,8 @@ async def start(bot, message):
         InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
         InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")
     ],[
-        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
-        InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
+        InlineKeyboardButton("🍿 MOVIES CHANNEL🍿", url="https://t.me/SearchEngineupdate"),
+        InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/TeraBoxMoviesSearch")
     ]]
     await message.reply(text=script.START.format(message.from_user.mention),
                         disable_web_page_preview=True,
@@ -66,8 +66,8 @@ async def misc(bot, update):
            InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
            InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")
        ],[
-           InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
-           InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
+           InlineKeyboardButton("🍿 MOVIES CHANNEL🍿", url="https://t.me/SearchEngineupdate"),
+           InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/TeraBoxMoviesSearch")
        ]]
        await update.message.edit(text=script.START.format(update.from_user.mention),
                                  disable_web_page_preview=True,
